ea728e0968dafc1bce0eefcc678a4afa409c693d
unknown <kevin.bernfeld@COHS_MAC5MD.olmhealth.com>
2015-05-08 11:17:18 -0400

    maybe???

--- src/slackbot/Controllers/stockscontroller.cs
+++ src/slackbot/Controllers/stockscontroller.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.Net.Http;
-using Newtonsoft;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
